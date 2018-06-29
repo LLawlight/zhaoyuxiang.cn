5160e7d969b39be98901a05fd98752b4b062b97a
LLawliet <553867163@qq.com>
2018-06-29 17:56:08 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: "egg",
       script: "npm",
-      cwd: "/zhaoyuxiang.cn-egg",
+      cwd: "./zhaoyuxiang.cn-egg",
       watch: true,
       args: "start"
     },
     {
       name: "nuxt",
       script: "npm",
-      cwd: "/zhaoyuxiang.cn-nuxt",
+      cwd: "./zhaoyuxiang.cn-nuxt",
       watch: true,
       args: "start"
     }
