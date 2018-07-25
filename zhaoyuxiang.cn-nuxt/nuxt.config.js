73631241429edc9ea20c8bc0d576e39079f119e9
L·Lawliet <553867163@qq.com>
2018-07-25 23:32:30 +0800

    修改网站ico

--- zhaoyuxiang.cn-nuxt/nuxt.config.js
+++ zhaoyuxiang.cn-nuxt/nuxt.config.js
@@ -5,8 +5,10 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Meta description' },
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
       { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
     ],
     script: [
       { src: 'https://unpkg.com/wired-elements@latest/dist/wired-elements.bundled.min.js' }
